7f2d0bda023d87a34bfaea8f5e90c0a058cc178e
Bhupinder Karan Singh <bhupinderkaransingh@gmail.com>
2022-08-25 19:30:11 -0400

    Initial commit

---
+++ src/main/java/com/example/mytwitter_backend/MyTwitterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytwitter_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTwitterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTwitterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mytwitter_backend/MyTwitterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytwitter_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTwitterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
