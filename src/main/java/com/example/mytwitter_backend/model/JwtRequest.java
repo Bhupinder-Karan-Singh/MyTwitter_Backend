13b8c80c33d6063518421c00a54455b5238363fe
Bhupinder Karan Singh <bhupinderkaransingh@gmail.com>
2022-08-26 00:51:26 -0400

    commit

---
+++ src/main/java/com/example/mytwitter_backend/model/JwtRequest.java
@@ -0,0 +1,4 @@
+package com.example.mytwitter_backend.model;
+
+public class JwtRequest {
+}
---
+++ src/main/java/com/example/mytwitter_backend/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.mytwitter_backend.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if(username.equals("bhupinderkaransingh@gmail.com")){
+            return new User("bhupinderkaransingh@gmail.com","12345678", new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("No user found");
+        }
+    }
+}
